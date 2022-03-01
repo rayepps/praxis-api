36c880655674bec9ab16c34d4c926a88bad3425a
Ray Epps <rayharryepps@gmail.com>
2022-03-01 02:27:38 -0700

    goodbye coralogix — hello logtail

--- src/core/config.ts
+++ src/core/config.ts
@@ -16,17 +16,13 @@ const config = {
   graphcmsApiUrl: get('GRAPHCMS_API_URL'),
   googleGeocodingApiKey: get('GOOGLE_GEOCODING_API_KEY'),
   segmentKey: get('SEGMENT_KEY'),
-  coralogixKey: get('CORALOGIX_API_KEY'),
-  coralogixUrl: get('CORALOGIX_URL'),
-  coralogixApplicationName: get('CORALOGIX_APPLICATION_NAME'),
-  coralogixSubsystemName: get('CORALOGIX_SUBSYSTEM_NAME'),
-  coralogixLoggerName: get('CORALOGIX_LOGGER_NAME'),
   mongoUsername: get('MONGO_USER_NAME'),
   mongoPassword: get('MONGO_PASSWORD'),
   mongoInstanceName: get('MONGO_INSTANCE_NAME'),
   mongoSubdomain: get('MONGO_SUBDOMAIN'),
   redirectorUrl: get('PRAXIS_REDIRECTOR_URL'),
-  redirectorApiKey: get('PRAXIS_REDIRECTOR_API_KEY')
+  redirectorApiKey: get('PRAXIS_REDIRECTOR_API_KEY'),
+  logtailToken: get('LOGTAIL_TOKEN')
 }
 
 export type Config = typeof config
